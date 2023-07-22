dc1a9e759de81b5c5662fc897f932cc7224566a7
Alex Ownejazayeri <alirezaownejazayeri@gmail.com>
2023-07-22 02:04:31 -0700

    it's late maybe i should go to bed instead of this

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,12 +1,14 @@
 const config = require('./webpack.config')
-const merge = require('webpack-merge')
-
+const webpackMerge = require('webpack-merge')
 const path = require('path')
+
+const { join } = path
+const { merge } = webpackMerge
 
 module.exports = merge(config, {
   mode: 'production',
 
   output: {
-    path: path.join(__dirname, 'static')
+    path: join(__dirname, 'static')
   }
 })
